75f0cc3969d3d76e037ee307f38da6b728faecaa
Buğra Görkem Özdağ <bgozdag@gmail.com>
2021-10-23 00:11:03 +0300

    set baud rate

--- main.py
+++ main.py
@@ -7,17 +7,19 @@ class App:
     def __init__(self) -> None:
         self.device = Bauer()
         self.mc = ModbusController()
-        logger.info(self.mc.read(self.device.manufacturer))
-        logger.info(self.mc.read(self.device.model))
-        logger.info(self.mc.read(self.device.options))
-        logger.info(self.mc.read(self.device.version))
-        logger.info(self.mc.read(self.device.serial_number))
+        logger.info("Manufacturer: {}".format(self.mc.read(self.device.manufacturer)))
+        logger.info("Model: {}".format(self.mc.read(self.device.model)))
+        logger.info("Options: {}".format(self.mc.read(self.device.options)))
+        logger.info("Version: {}".format(self.mc.read(self.device.version)))
+        logger.info("Serial number: {}".format(self.mc.read(self.device.serial_number)))
     
-    def set_baud_rate(self, target_baud_rate):
+    def set_baud_rate(self):
         current_baud_rate = self.mc.read(self.device.baud_rate)
-        logger.info(current_baud_rate)
-        # if current_baud_rate != target_baud_rate:
-        #     self.mc.
+        logger.info("Baud rate: {}".format(current_baud_rate))
+        if current_baud_rate != self.device.MAX_BAUD_RATE:
+            self.mc.write(self.device.baud_rate, self.device.MAX_BAUD_RATE)
+            logger.info("Baud rate set to: self.device.MAX_BAUD_RATE")
+            self.mc.reset_connection()
 
 
 if __name__ == "__main__":
